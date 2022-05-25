87a9be22ae8f1866cfeeb97a9bb8108e277fb5fd
Redsanker <95141261+Redsanker@users.noreply.github.com>
2022-05-25 11:42:29 +0200

    update

--- connexion.js
+++ connexion.js
@@ -1,10 +1,7 @@
-/* Moralis init code */
+
 const serverUrl = "https://ynxef7bnorq4.usemoralis.com:2053/server";
 const appId = "3zcgZ9rsrJjwChyaOHEKrUexr20bQpid3Qkl7n6e";
 Moralis.start({ serverUrl, appId });
-
-/* TODO: Add Moralis Authentication code */
-
 
 submit = document.querySelector("[name=valid_form]");
 
@@ -18,23 +15,10 @@ email_input = document.querySelector("[name=email]")
 
 pin_input = document.querySelector("[name=pin]")
 
+
 console.log(submit)
 
-// submit.onclick = addUser(username_input.value, birthday_input.value, genre_input.value, "wip", email_input.value, pin_input.value)
-
-// function addUser(username, date, genre, status, email, pin) {
-//     console.log("adduser function")
-//     const user = new Moralis.User();
-//     user.set("username", username);
-//     user.set("birthday", date);
-//     user.set("genre", genre);
-//     user.set("status", status);
-//     user.set("email", email);
-//     user.set("password", pin);
-//     user.signUp();
-// }
-
-function signUp(username, date, genre, status, email, pin) {
+async function signUp(username, date, genre, status, email, pin) {
     const user = new Moralis.User();
     user.set("username", username);
     user.set("birthday", date);
@@ -43,8 +27,36 @@ function signUp(username, date, genre, status, email, pin) {
     user.set("email", email);
     user.set("password", pin);
 
-    user.signUp();
-    console.log("Sign up successfull for ", username)
+    try {
+    await user.signUp();
+    await popUpSignUp();
+    } catch (error) {
+    // Show the error message somewhere and let the user try again.
+    Swal.fire("Error: " + error.code + " " + error.message);
+    }
 }
+submit.onclick = () => signUp(username_input.value, birthday_input.value, genre_input.value, "wip", email_input.value, pin_input.value)
 
-submit.addEventListener("click", signUp(username_input.value, birthday_input.value, genre_input.value, "wip", email_input.value, pin_input.value))
+// submit.onclick = () => for (username_input.value != "" && birthday_input.value != "" && email_input.value != ""  && pin_input.value) {
+//     submit.onclick = () => popUpSignUp()
+// }
+
+
+function popUpSignUp() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    
+    Toast.fire({
+        icon: 'success',
+        title: 'Inscription réussie'
+    })
+}
